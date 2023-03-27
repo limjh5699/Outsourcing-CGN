f001751b1b09cfb336a04092c316c3f4a28fa406
limjh5699 <limjh5699@gmail.com>
2023-03-27 17:36:28 +0900

    FEAT: Import thirdSection

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -1,11 +1,13 @@
 import FirstSection from "../components/main/mainPage/firstSection/firstSection";
 import SecondSection from "../components/main/mainPage/secondSection/secondSection";
+import ThirdSection from "../components/main/mainPage/thirdSection/thirdSection";
 
 const MainPage = () => {
   return (
     <div>
       <FirstSection />
       <SecondSection />
+      <ThirdSection />
     </div>
   );
 };
