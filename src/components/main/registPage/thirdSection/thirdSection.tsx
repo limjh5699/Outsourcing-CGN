406b364e795a2bd6bb3f095ffaab07557d947904
limjh5699 <limjh5699@gmail.com>
2023-03-28 17:06:52 +0900

    FEAT: Add ThirdSection at Regist Page

---
+++ src/components/main/registPage/thirdSection/thirdSection.tsx
@@ -0,0 +1,51 @@
+import styles from "./thirdSection.module.css";
+
+const ThirdSection = () => {
+  return (
+    <div className={styles.background}>
+      <div className={styles.contents}>
+        <div className={styles.top}>
+          <p className={styles.title}>[무료2] 나는 솔로</p>
+        </div>
+        <div className={styles.mid}>
+          <h1>개인 참가자 신청하기</h1>
+          <p>
+            [FMC 2023]은 개인별로도 참가하실 수 있습니다 <br /> 단, 출석교회를
+            퐁당 네트워크 교회로 가입시키면 무료로 참여하실 수 있습니다
+          </p>
+          <div className={styles.container}>
+            <div className={styles.box}>
+              <p>
+                출석 교회를
+                <br />
+                퐁당 네트워크
+                <br />
+                교회로 가입
+              </p>
+              <div>무료</div>
+            </div>
+            <div className={styles.box}>
+              <p>
+                개인 참가자
+                <br /> 등록
+              </p>
+              <span>30,000원</span>
+            </div>
+            <div className={styles.box}>
+              <p>
+                당일 현장
+                <br /> 등록
+              </p>
+              <span>30,000원</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.bottom}>
+          <button>퐁당 네트워크 교회 가입 신청서 작성</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ThirdSection;
--- src/pages/registPage.tsx
+++ src/pages/registPage.tsx
@@ -1,11 +1,13 @@
 import FirstSection from "../components/main/registPage/firstSection/firstSection";
 import SecondSection from "../components/main/registPage/secondSection/secondSection";
+import ThirdSection from "../components/main/registPage/thirdSection/thirdSection";
 
 const RegistPage = () => {
   return (
     <div>
       <FirstSection />
       <SecondSection />
+      <ThirdSection />
     </div>
   );
 };
