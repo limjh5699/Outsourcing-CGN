896e1d85312db9330e7f03090e2f8e34ff6218fe
limjh5699 <limjh5699@gmail.com>
2023-03-30 21:25:16 +0900

    STYLE:  Responsive Web Design to Header and Footer

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,34 +1,66 @@
+import { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import styles from "./header.module.css";
 
 import logo from "../../../assets/layout/logo.png";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
 const Header = () => {
+  const [openBar, setOpenBar] = useState(false);
+  const notBarRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handler = (e: any) => {
+      if (notBarRef.current && notBarRef.current.contains(e.target)) {
+        setOpenBar(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handler);
+
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+  });
+
+  const onClickHandler = () => {
+    setOpenBar(!openBar);
+  };
+
   return (
     <header className={styles.background}>
+      {openBar && <div className={styles.notBar} ref={notBarRef}></div>}
       <div className={styles.container}>
         <img src={logo} alt="logo" className={styles.logo} />
-        <ul className={styles.nav}>
-          <li className={styles.li_right}>
+        <ul className={openBar === false ? styles.nav : styles.openNav}>
+          <li>
             <Link to={"/"}>행사소개</Link>
           </li>
-          <li className={styles.li_center}>
+          <li>
             <Link to={"/regist"}>참가 신청</Link>
           </li>
-          <li className={styles.li_left}>
+          <li>
             <Link to={"/time"}>타임 테이블</Link>
           </li>
-          <li className={styles.li_left}>
+          <li>
             <Link to={"/FAQ"}>자주 묻는 질문</Link>
           </li>
-          <li className={styles.li_center}>
+          <li>
             <Link to={"/place"}>행사장</Link>
           </li>
-          <li className={styles.li_left}>
+          <li>
             <Link to={"/event"}>이벤트</Link>
           </li>
         </ul>
+        <FontAwesomeIcon
+          onClick={onClickHandler}
+          className={styles.burger}
+          icon={faBars}
+          style={{ color: "#ffffff" }}
+        />
       </div>
     </header>
   );
